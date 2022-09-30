eb3744f73a38ab8d813581b06b121e2cb4fa527a
Gabriel Villaruel <gvill005@gmail.com>
2022-09-30 16:29:02 -0700

    redid the app with no notes

--- src/App.js
+++ src/App.js
@@ -1,89 +1,83 @@
-import './App.css';
+import './App.css'
 import React, { useState } from "react"
 
+const App = () => {
 
-function App() {
-  const [ formData, setFormData ] = useState({
-    email: "",
-    password: "",
-    confirmPassword: "",
-    joinedNewsletter: false
-  })
+  const [ formData, setFormData ] = useState(
+    {
+      email: "",
+      password: "",
+      confirmPassword: "",
+      joinedNewsletter: true
+    }
+  )
+
 
   const handleChange = (event) => {
-    const { value, type, name, checked } = event.target
-    setFormData(prevFormData => (
-      {
-        ...prevFormData,
-        [name]: type === "checkbox" ? checked : value
+    const { name, type, value, checked } = event.target
+    setFormData(prevFormData => ({
+      ...prevFormData,
+      [name]: type === "checkbox" ? checked : value
       }
     ))
   }
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    
     if(formData.password === formData.confirmPassword){
-      console.log("Successfully signed up!")
-    } else{
-      console.log("Passwords do not match")
+      alert("Successfully signed up!")
+    } else {
+      alert("Passwords do not match")
       return
-    } 
-
-    if(formData.joinedNewsletter === true){
-      console.log("Thanks for subscribing!")
     }
 
-  } 
+    {formData.joinedNewsletter && alert("Thanks for subscribing!")}
+  }
 
 
   return (
-   
     <div className="form-container">
-
-       
       <form className="form" onSubmit={handleSubmit}>
-
-        <input
-         type="text"
-         placeholder="Email"
-         className="form--input" 
-         value={formData.email} 
-         name="email" 
-         onChange={handleChange}/>
-
-        <input
-         type="text" 
-         placeholder="Password" 
-         className="form--input"
-         value={formData.password} 
-         name="password" 
-         onChange={handleChange}/>
-
-        <input
-         type="text" 
-         placeholder="Confirm Password" 
-         className="form--input"
-         value={formData.confirmPassword} 
-         name="confirmPassword" 
-         onChange={handleChange}/>
-
-        <div className="form--marketing">
-          <input
-           id="okayToEmail" 
-           type="checkbox" 
-           checked={formData.joinedNewsletter} 
-           name="joinedNewsletter" 
-           onChange={handleChange}/>
-          <label htmlFor="okayToEmail">Join our newsletter!</label>
-        </div>
-        <div className="button">
-          <button className="form--submit">Sign Up</button>
-        </div>
-        
+        <input 
+          name="email" 
+          placeholder="Email" 
+          type="text"
+          onChange={handleChange} 
+          value={formData.email}
+          className="form--input"/>
+          <input 
+          name="password" 
+          placeholder="Password" 
+          type="text"
+          onChange={handleChange} 
+          value={formData.password}
+          className="form--input"/>
+          <input 
+          name="confirmPassword" 
+          placeholder="Confirm password" 
+          type="text"
+          onChange={handleChange} 
+          value={formData.confirmPassword}
+          className="form--input"/>
+          <div className='form--marketing'>
+            <input 
+            id="okayToEmail"
+            name="joinedNewsletter" 
+            type="checkbox"
+            onChange={handleChange} 
+            checked={formData.joinedNewsletter}
+            />
+            <label htmlFor="okayToEmail">Join our newsletter!</label>
+          </div>
+          <div className="button">
+            <button className="form--submit">Submit!</button>
+          </div>
+          
       </form>
-
     </div>
-  );
+  )
+
 }
 
-export default App;
+export default App
